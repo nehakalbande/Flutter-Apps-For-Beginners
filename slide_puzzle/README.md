ab8d6e1f7b9d96ee020e6e9095278ec477a0a3bc
Neha Kalbande <neha.leenaharsh@gmail.com>
2022-02-02 16:50:25 +0530

    first commit

---
+++ slide_puzzle/README.md
@@ -0,0 +1,3 @@
+A slide (15) puzzle implemented in Dart and Flutter.
+
+![GitHub Logo](docs/screen_shot.png)
---
+++ slide_puzzle/lib/src/themes.dart
@@ -0,0 +1,17 @@
+// Copyright 2020, the Flutter project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:slide_puzzle/src/theme_scenery.dart';
+
+import 'theme_plaster.dart';
+import 'theme_scenery.dart';
+import 'theme_seattle.dart';
+import 'theme_simple.dart';
+
+const themes = [
+  ThemeSimple(),
+  ThemeSeattle(),
+  ThemePlaster(),
+  ThemeScenery(),
+];
